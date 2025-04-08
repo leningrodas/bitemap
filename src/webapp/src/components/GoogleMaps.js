eb70181e2129d746d8249b631c6b83b2fbf685b0
lening rodas <Lening.a.rodas@gmail.com>
2025-04-08 17:41:24 -0500

    removed sensitive info

--- src/webapp/src/components/GoogleMaps.js
+++ src/webapp/src/components/GoogleMaps.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { APIProvider, Map } from '@vis.gl/react-google-maps';
 
 const GoogleMaps = () => {
+ console.log("Google Maps API Key:", process.env.REACT_APP_GOOGLE_MAPS_KEY);
     return (
-        <APIProvider apiKey="AIzaSyDnS_n0GA4PA5poHnuREl-_IwflhaC1Fq4">
+        <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_KEY}>
             <Map
                 style={{ width: '100vw', height: '100vh' }}
                 defaultCenter={{ lat: 40.749933, lng: -73.98633 }}
